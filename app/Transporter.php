5e36c6edce692821ba6b08bc9f0bd2b546eeef85
SahanHerath <sahand.herath@gmail.com>
2019-08-01 03:43:06 +0530

    Create :CreateTransporter view

--- app/Transporter.php
+++ app/Transporter.php
@@ -8,6 +8,6 @@ class Transporter extends Model
 {
     //
     protected $fillable = [
-        'Transport_Service','Address', 'Contact_No', 'Link','Description','driver','decoration','rent_hours','rent_km','cost','extra_km','extra_hour','Main_pic','pic1','pic2','pic3','pic4','user_id'
+        'Transport_Service','Address', 'Contact_No', 'Link','Description','driver','decoration','rent_hours','Main_pic','pic1','pic2','pic3','pic4','user_id','rent_km'
     ];
 }
