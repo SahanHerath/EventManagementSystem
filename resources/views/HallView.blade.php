c7cc8e072fb17c1f05452cc937313284b6250226
SahanHerath <sahand.herath@gmail.com>
2019-09-01 07:25:16 +0530

    hallview add table arrangements

--- resources/views/HallView.blade.php
+++ resources/views/HallView.blade.php
@@ -125,121 +125,121 @@
 				</div><!-- col-sm-12 -->
                 
                 <div class="row">
-				<div class="col-sm-6 col-md-6 col-lg-4">
+				<div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin">
-                        <img src="uploads/hall features/projection.jpg" width="42" height="42">
+                        <img src="images/hall features/projection.jpg" width="42" height="42">
 						<h5 class="margin-tb-20"><b>Projection</b></h5>
 						
 					</div><!-- counter -->
 				</div><!-- col-md-4 -->
 				
-				<div class="col-sm-6 col-md-6 col-lg-4">
+				<div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/wifi.jpg"  width="42" height="42">
+                        <img src="images/hall features/wifi.jpg"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Wireless Internet</b></h5>
 						
 					</div><!-- counter -->
 				</div><!-- col-md-4 -->
 				
-				<div class="col-sm-6 col-md-6 col-lg-4">
+				<div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/parking.png"  width="42" height="42">
+                        <img src="images/hall features/parking.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Parking</b></h5>
 						
 					</div><!-- counter -->
 				</div><!-- col-md-4 -->
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/securitycamera.png"  width="42" height="42">
+                        <img src="images/hall features/securitycamera.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Security Camera</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/security personal.png"  width="42" height="42">
+                        <img src="images/hall features/security personal.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Security Personal</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/rec.png"  width="42" height="42">
+                        <img src="images/hall features/rec.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Reception Area</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/bar.png"  width="42" height="42">
+                        <img src="images/hall features/bar.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Bar</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/garden.png"  width="42" height="42">
+                        <img src="images/hall features/garden.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Garden</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/smoking.png"  width="42" height="42">
+                        <img src="images/hall features/smoking.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Smoking Area</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/welcomedrink.png"  width="42" height="42">
+                        <img src="images/hall features/welcomedrink.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Welcome Drink</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/buffet.png"  width="42" height="42">
+                        <img src="images/hall features/buffet.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Buffet</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/wheelchair.png"  width="42" height="42">
+                        <img src="images/hall features/wheelchair.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Handicap accessible</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/Varenda.png"  width="42" height="42">
+                        <img src="images/hall features/Varenda.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Varenda</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/balcony.png"  width="42" height="42">
+                        <img src="images/hall features/balcony.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Balcony</b></h5>
 						
 					</div><!-- counter -->
 				</div>
 
-                <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="col-sm-6 col-md-6 col-lg-3">
 					<div class="iconbox margin-b-30">
-                        <img src="uploads/hall features/stage.png"  width="42" height="42">
+                        <img src="images/hall features/stage.png"  width="42" height="42">
 						<h5 class="margin-tb-20"><b>Stage</b></h5>
 						
 					</div><!-- counter -->
@@ -249,6 +249,119 @@
 		</div><!-- container -->
 	</section><!-- progression-section -->
 
+
+	
+    <section class=" section">
+		<div class="container">
+			
+                <div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Table Arrangements</b></h3>
+					</div>
+				</div><!-- col-sm-12 -->
+                
+                <div class="row">
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin">
+                        <img src="images/table arrangements/Theatre1.jpg" width="80" height="100">
+						<h5 class="margin-tb-20"><b>Theatre</b></h5>
+						
+					</div><!-- counter -->
+				</div><!-- col-md-4 -->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/Ushape1.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>U Shape</b></h5>
+						
+					</div><!-- counter -->
+				</div><!-- col-md-4 -->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/VShape.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>V Shape</b></h5>
+						
+					</div><!-- counter -->
+				</div><!-- col-md-4 -->
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/banquet.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Banquet</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/caberet.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Caberet</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/Classroom.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Classroom</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/Boardroom.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Boardroom</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/HallowSquare.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Hallow Square</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/Herringbone.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Herringbone</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/TopTable.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>TopTable & Springs</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/informal.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Informal</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                <div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="iconbox margin-b-30">
+                        <img src="images/table arrangements/Oval.jpg"  width="80" height="100">
+						<h5 class="margin-tb-20"><b>Oval</b></h5>
+						
+					</div><!-- counter -->
+				</div>
+
+                
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- progression-section -->
 	
 	<footer>
 		<p class="copyright">
