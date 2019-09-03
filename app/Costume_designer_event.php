fdb8148de3e2d6ae378f7640f7c0267009d6974f
Yashodha1996 <rmynilmini5@gmail.com>
2019-09-03 20:41:11 +0530

    Costume designer model add

---
+++ app/Costume_designer_event.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Costume_designer_event extends Model
+{
+    //
+    protected $fillable = [
+        'id','user_id','Wedding','Party','fashion_show','sports','Coperate_event','created_at','updated_at'
+
+    ];
+}
