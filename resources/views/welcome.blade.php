412bb49cb8a198ebe4191d949286949466c39ac4
Shamith12 <shamithathauda@gmail.com>
2019-09-05 01:39:44 +0530

    main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -222,6 +222,28 @@
         </div>
       </div>
 
+      <div class="site-blocks-cover overlay" style="background-image: url(images/costume.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade">
+              <h1 class="mb-2">Costume Designers</h1>
+              <h2 class="caption">Make it as your wish</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="site-blocks-cover overlay" style="background-image: url(images/poruwa111.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7 text-center" data-aos="fade">
+              <h1 class="mb-2">Poruwa Rituals</h1>
+              <h2 class="caption">Make it as your wish</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
     
 
     </div>
