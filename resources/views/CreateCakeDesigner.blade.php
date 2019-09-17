a9e11ba742167d70acf1a6984985f955a765d935
Anuka1995 <anuksanjay@gmail.com>
2019-09-18 03:16:02 +0530

    cake design form frontend parts

--- resources/views/CreateCakeDesigner.blade.php
+++ resources/views/CreateCakeDesigner.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="background-image: url('images/cake26.jpg'); background-attachment: fixed;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Add Cake Designers Details</div>
+                <center class="card-header">Add Cake Designers Details</center>
 
                 <div class="card-body">
                     <form method="POST" action="/addCakeDesigner" enctype="multipart/form-data">
@@ -222,7 +222,7 @@
                         
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-5">
                                 <button type="submit" class="btn btn-primary">
                                     Submit
                                 </button>
