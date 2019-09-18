49da189c4c33ddff5ea8a1dac09f47f6507d46ec
Anuka1995 <anuksanjay@gmail.com>
2019-09-18 22:55:14 +0530

    dance form frontend parts

--- resources/views/CreateDancing.blade.php
+++ resources/views/CreateDancing.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="background-image: url('images/form_dance1.jpg'); background-attachment: fixed;">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card" style="background: rgba(255,255,255,0.85);">
                 <div class="card-header">Add Dancing Team/Group </div>
 
                 <div class="card-body">
@@ -299,7 +299,7 @@
                         
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-5">
                                 <button type="submit" class="btn btn-primary">
                                     Submit
                                 </button>
