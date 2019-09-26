c9f76c225d0b9db50704c35b48b34f024671949e
SahanHerath <sahand.herath@gmail.com>
2019-09-26 09:35:21 +0530

    Change password in Cake(modal)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -171,7 +171,7 @@ class UsersController extends Controller
     {
         if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
             // The passwords matches
-            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+            return redirect()->back()->with('warning_message',"Your current password does not matches with the password you provided. Please try again.");
         }
         if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
             //Current password and new password are same
@@ -185,7 +185,7 @@ class UsersController extends Controller
         $user = Auth::user();
         $user->password = bcrypt($request->get('new-password'));
         $user->save();
-        return redirect('/Profile')->with('flash_message','Password changed successfully');
+        return redirect()->back()->with('flash_message','Password changed successfully');
     }
 
 }
