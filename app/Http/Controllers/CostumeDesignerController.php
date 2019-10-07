71a06433d8602a6a4be7a3314342087c02b5be04
SahanHerath <sahand.herath@gmail.com>
2019-10-07 23:20:32 +0530

    rating in Costume user profile

--- app/Http/Controllers/CostumeDesignerController.php
+++ app/Http/Controllers/CostumeDesignerController.php
@@ -251,6 +251,7 @@ class CostumeDesignerController extends Controller
                 ->join('costume_designers','users.id','=','costume_designers.user_id')
                 ->join('costume_designer_events','users.id','=','costume_designer_events.user_id')
                 ->where('category','=','Cloth_Designers')
+                ->select('users.id as userid','users.name','email','costume_designers.id as costumeid','costume_designers.Name','Address','Contact_No','Link','Description','wedding_dress_designs','clothing_orders','sport_kit_designs','saree_work','traditional_dress','gents_wear','ladies_wear','gents_foot_wear','ladies_foot_wear','sports_shoes','Main_pic','pic1','pic2','pic3','pic4','costume_designer_events.id as eventid','Wedding','Party','fashion_show','sports','Coperate_event')
                 ->get();
 
         $deto=DB::table('users')
@@ -259,7 +260,71 @@ class CostumeDesignerController extends Controller
                 ->select('costume_packages.id','Package_Name', 'Event_Type', 'Services','Price','Pdf')
                 ->get();
 
-                return view('CostumeDesignerView',compact('data','deto'));
+                $rate=DB::table('users')
+                ->join('ratings','ratings.user_id','=','users.id')
+                ->where('users.id','=',$id)
+                ->where('blocked','=',"0")
+                ->select('ratings.id','rating','Comment','ratings.Email','image','ratings.created_at','user_name')
+                ->get();
+    
+        $average=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->avg('rating');
+    
+        $one=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->where('rating','=','1')
+                   ->count();
+    
+        $two=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->where('rating','=','2')
+                   ->count();
+    
+        $three=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->where('rating','=','3')
+                   ->count();
+    
+        $four=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->where('rating','=','4')
+                   ->count();
+    
+        $five=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->where('rating','=','5')
+                   ->count();
+    
+        $all=DB::table('ratings')
+                   ->where('ratings.user_id','=',$id)
+                   ->where('blocked','=',"0")
+                   ->count();
+    
+            if($all!=0)
+            {
+                $precentage1=$one/$all*100;
+                $precentage2=$two/$all*100;
+                $precentage3=$three/$all*100;
+                $precentage4=$four/$all*100;
+                $precentage5=$five/$all*100;
+            }
+            else 
+            {
+                $precentage1=0;
+                $precentage2=0;
+                $precentage3=0;
+                $precentage4=0;
+                $precentage5=0;
+            }
+
+        return view('CostumeDesignerView',compact('data','deto','average','rate','all','one','two','three','four','five','precentage1','precentage2','precentage3','precentage4','precentage5'));
     }
 
     public function wedding()
