3bb8afcdb5d900b1b7f2fb087d55eb764798e1bd
Rumith55 <rumithwitharana5@gmail.com>
2019-10-09 23:58:27 +0530

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,101 +1,225 @@
-@extends('layouts.app')
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Evora</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container" style="background-attachment: fixed;">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card" style="background: rgba(255,255,255,0.85);">
-                <div class="card-header">Your Suggestion</div>
+        <link rel="shortcut icon" type="image/png" href="rumi/images/favicon.png">
+        <!-- Place favicon.ico in the root directory -->
 
-                <div class="card-body">
-                    <form method="POST" action="/addSuggestion" enctype="multipart/form-data">
-                        @csrf
+        <!-- all css here -->
+        <!-- bootstrap v3.3.7 css -->
+        <link rel="stylesheet" href="rumi/css/bootstrap.min.css">
+        <!-- animate css -->
+        <link rel="stylesheet" href="rumi/css/animate.css">
+        <!-- owl.carousel.2.0.0-beta.2.4 css -->
+        <link rel="stylesheet" href="rumi/css/owl.carousel.css">
+        <!-- font-awesome v4.6.3 css -->
+        <link rel="stylesheet" href="rumi/css/font-awesome.min.css">
+        <!-- magnific-popup.css -->
+        <link rel="stylesheet" href="rumi/css/magnific-popup.css">
+        <!-- flaticon.css -->
+        <link rel="stylesheet" href="rumi/css/flaticon.css">
+        <!-- slicknav.min.css -->
+        <link rel="stylesheet" href="rumi/css/slicknav.min.css">
+        <!-- style css -->
+        <link rel="stylesheet" href="rumi/css/styles.css">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="rumi/css/responsive.css">
+        <!-- modernizr css -->
+        <script src="rumi/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
-                        <div class="form-group row{{ $errors->has('Name') ? ' has-error' : '' }}">
-                            <label for="Name" class="col-md-4 col-form-label text-md-right">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="Name" type="text" class="form-control @error('Name') is-invalid @enderror" name="Name" value="{{ old('Name') }}"  autocomplete="Name" autofocus title="Fill this field">
-
-                                @if ($errors->has('Name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('Name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+        <!-- header-area start -->
+        <header class="header-area" id="sticky-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="header-bottom">
+                             <div class="row">
+                                <div class="col-md-3 col-sm-9 col-xs-6">
+                                    <div class="logo">
+                                        <a href="/">
+                                            Evora
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 hidden-sm hidden-xs">
+                                    <div class="mainmenu text-right">
+                                        <ul id="navigation">
+                                            <!-- dhagdagdagdhsj -->
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-1 col-sm-2 col-xs-4">
+                                    <div class="search-wrap text-right">
+                                        <ul>
+                                            <li><a href="cart.html"><i class="fa fa-shopping-cart"></i><span>3</span></a></li>
+                                            <li><a href="javascript:void(0);"><i class="fa fa-search"></i></a>
+                                                <ul>
+                                                    <li>
+                                                        <form action="#">
+                                                            <input type="text" placeholder="Search Here...">
+                                                            <button><i class="fa fa-search"></i></button>
+                                                        </form>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class=" col-xs-2 col-sm-1 hidden-md hidden-lg">
+                                    <div class="responsive-menu-wrap floatright"></div>
+                                </div>
+                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- header-area end -->
 
-                        <div class="form-group row{{ $errors->has('Email') ? ' has-error' : '' }}">
-                            <label for="Email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="Email" type="Email" class="form-control @error('Email') is-invalid @enderror" name="Email" value="{{ old('Email') }}"  autocomplete="Email" title="Fill this field">
-
-                                @if ($errors->has('Email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('Email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+        <!-- breadcumb-area start -->
+        <div class="breadcumb-area bg-img-5 black-opacity">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="breadcumb-wrap text-center">
+                            <h2>Contact us</h2>
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li>/</li>
+                                <li class="active">contact</li>
+                            </ul>
                         </div>
-
-                        <div class="form-group row{{ $errors->has('Contact_No') ? ' has-error' : '' }}">
-                            <label for="Contact_No" class="col-md-4 col-form-label text-md-right">Contact Number</label>
-
-                            <div class="col-md-6">
-                                <input id="Contact_No" type="text" class="form-control @error('Contact_No') is-invalid @enderror" name="Contact_No" value="{{ old('Contact_No') }}" autocomplete="Contact_No" title="Fill this field with Contact Number">
-
-                                @if ($errors->has('Contact_No'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('Contact_No') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row{{ $errors->has('Regarding') ? ' has-error' : '' }}">
-                            <label for="Regarding" class="col-md-4 col-form-label text-md-right">Regarding</label>
-                            
-                            <div class="col-md-6">
-                                <select id="Regarding" class="form-control" name="Regarding" value="{{ old('Regarding') }}" title="Fill this field with Avalability" >
-                                    <option value="" disabled selected>--Select Type--</option>
-                                    <option value="Financial">Financial</option>
-                                    <option value="Services">Services</option>
-                                    <option value="Community">Community</option>
-                                    <option value="Development">Development</option>
-                                    <option value="General Info">General Info</option>
-                                    <option value="Advertising">Advertising</option>
-                                    <option value="Website Bug">Website Bug</option>
-                                    <option value="Donations">Donations</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        
-
-                        <div class="form-group{{ $errors->has('Message') ? ' has-error' : '' }}">
-                        <div class="form-group row">
-                            <label for="Message" class="col-md-4 col-form-label text-md-right">Message</label>
-                            
-                            <div class="col-md-6">
-                                <textarea name='Message' cols='50' rows='5' id='Message' class="form-control @error('Message') is-invalid @enderror" title="Fill this field."></textarea>
-                            </div>
-                        </div>
-                        </div>
-
-      
-
-                <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-5">
-                                <button type="submit" class="btn btn-primary">
-                                    Submit Suggestion
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <!-- breadcumb-area end -->
+
+        <!-- contact-area start -->
+        <div class="contact-area bg-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <div class="contact-form">
+                            <div class="cf-msg"></div>
+                            <form action="mail.php" method="post" id="cf">
+                                <div class="row">
+                                    <div class="col-md-6 col-xs-12">
+                                        <input type="text" placeholder="Name" id="fname" name="fname">
+                                    </div>
+                                    <div class="col-md-6 col-xs-12">
+                                        <input type="text" placeholder="Email" id="email" name="email">
+                                    </div>
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <input type="text" placeholder="Subject" id="subject" name="subject">
+                                    </div>
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <textarea class="contact-textarea" placeholder="Message" id="msg" name="msg"></textarea>
+                                    </div>
+                                    <div class="col-md-12 col-sm-12 col-xs-12">
+                                        <button id="submit" class="cont-submit btn-contact btn-style" name="submit">SEND MESSAGE</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="contact-wrap">
+                            <ul>
+                                <li>
+                                    <i class="fa fa-phone"></i>
+                                    Phone number
+                                    <p>
+                                        <span>+ (0012) 123 456 789</span>
+                                        <span>+ (0012) 123 456 789</span>
+                                    </p>
+                                </li>
+                                <li>
+                                    <i class="fa fa-envelope"></i>
+                                    Email Id
+                                    <p>
+                                        <span>info145@gmail.com</span>
+                                        <span>info145@gmail.com</span>
+                                    </p>
+                                </li>
+                                <li>
+                                    <i class="fa fa-location-arrow"></i>
+                                    Location
+                                    <p>
+                                        <span>+227 Marion Street Address Here Columbia, SC 29201</span>
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="googleMap"></div>
+        </div>
+        <!-- contact-area end -->
+
+        <!-- footer-area start -->
+        
+        <!-- footer-area end -->
+
+		<!-- all js here -->
+		<!-- jquery latest version -->
+        <script src="rumi/js/vendor/jquery-1.12.4.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="rumi/js/bootstrap.min.js"></script>
+		<!-- owl.carousel.2.0.0-beta.2.4 css -->
+        <script src="rumi/js/owl.carousel.min.js"></script>
+		<!-- counterup.main.js -->
+        <script src="rumi/js/counterup.main.js"></script>
+		<!-- isotope.pkgd.min.js -->
+        <script src="rumi/js/imagesloaded.pkgd.min.js"></script>
+		<!-- isotope.pkgd.min.js -->
+        <script src="rumi/js/isotope.pkgd.min.js"></script>
+		<!-- jquery.waypoints.min.js -->
+        <script src="rumi/js/jquery.waypoints.min.js"></script>
+		<!-- jquery.magnific-popup.min.js -->
+        <script src="rumi/js/jquery.magnific-popup.min.js"></script>
+		<!-- jquery.slicknav.min.js -->
+        <script src="rumi/js/jquery.slicknav.min.js"></script>
+		<!-- wow js -->
+        <script src="rumi/js/wow.min.js"></script>
+		<!-- plugins js -->
+        <script src="rumi/js/plugins.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbeBYsZSDkbIyfUkoIw1Rt38eRQOQQU0o"></script>
+        <script>
+            function initialize() {
+                var mapOptions = {
+                    zoom: 15,
+                    scrollwheel: false,
+                    center: new google.maps.LatLng(40.712764, -74.005667),
+                    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#222222"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#222222"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#222222"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#222222"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#222222"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#222222"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#222222"},{"lightness":17}]}]
+                };
+
+                var map = new google.maps.Map(document.getElementById('googleMap'),
+                    mapOptions);
+
+
+                var marker = new google.maps.Marker({
+                    position: map.getCenter(),
+                    animation: google.maps.Animation.BOUNCE,
+                    map: map
+                });
+
+            }
+
+            google.maps.event.addDomListener(window, 'load', initialize);
+        </script>
+		<!-- main js -->
+        <script src="rumi/js/scripts.js"></script>
+    </body>
+</html>
