9c4ce457eea4e74c307d22628fa4fb702aa943af
Anuka1995 <anuksanjay@gmail.com>
2019-10-12 13:09:13 +0530

    search bar for cake event plannerview

--- resources/views/EventPlanner.blade.php
+++ resources/views/EventPlanner.blade.php
@@ -27,7 +27,8 @@
     <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="css/style.css">
-    
+    <link rel="stylesheet" href="css/navbar.css">
+
   </head>
   <body>
   
@@ -209,7 +210,11 @@
     </div>
 
     
-    
+    <div class="topnav">
+    <button type="submit" >Search</button>
+      <input type="text" placeholder="Search..">
+      
+    </div>
 
     
     <div class="site-section border-top">
