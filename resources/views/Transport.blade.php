30ad951c2bf3334318a95c5e125908b406e727ad
Anuka1995 <anuksanjay@gmail.com>
2019-10-12 13:15:40 +0530

    search bar for transportview

--- resources/views/Transport.blade.php
+++ resources/views/Transport.blade.php
@@ -28,6 +28,8 @@
 
     <link rel="stylesheet" href="css/style.css">
     
+    <link rel="stylesheet" href="css/navbar.css">
+    
   </head>
   <body>
   
@@ -199,7 +201,12 @@
         
       </div>
     </div>
-    
+    <div class="topnav">
+    <button type="submit" >Search</button>
+      <input type="text" placeholder="Search..">
+      
+    </div>
+
     <div class="site-section bg-light">
         <div class="container">
           <div class="row">
@@ -298,39 +305,6 @@
       </div>
       
     </div>
-    
-
-    <!-- <div class="py-5 quick-contact-info">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4 text-center">
-            <div>
-              <span class="icon-room text-white h2 d-block"></span>
-              <h2>Location</h2>
-              <p class="mb-0">New York - 2398 <br>  10 Hadson Carl Street</p>
-            </div>
-          </div>
-          <div class="col-md-4 text-center">
-            <div>
-              <span class="icon-clock-o text-white h2 d-block"></span>
-              <h2>Service Times</h2>
-              <p class="mb-0">Wednesdays at 6:30PM - 7:30PM <br>
-              Fridays at Sunset - 7:30PM <br>
-              Saturdays at 8:00AM - Sunset</p>
-            </div>
-          </div>
-          <div class="col-md-4 text-center">
-            <div>
-              <span class="icon-comments text-white h2 d-block"></span>
-              <h2>Get In Touch</h2>
-              <p class="mb-0">Email: info@yoursite.com <br>
-              Phone: (123) 3240-345-9348 </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
     
     <footer class="footer_area p_30">
 			<div class="container">
