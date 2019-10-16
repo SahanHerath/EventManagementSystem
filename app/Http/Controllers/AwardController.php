d6ac372e51fc8e55835ed242905381bc82886a95
Shamith12 <shamithathauda@gmail.com>
2019-10-16 11:23:35 +0530

    admin ,award,costumedesigner 1

--- app/Http/Controllers/AwardController.php
+++ app/Http/Controllers/AwardController.php
@@ -253,4 +253,15 @@ class AwardController extends Controller
 
         return view('awards.cake',compact('data'));
     }
+    public function Costume()
+    {
+        //
+        $data=DB::table('users')
+             ->join('awards','users.id','=','awards.user_id')
+             ->where('category','=','Cloth_Designers')
+             ->select('awards.id as awardid','users.id as userid','Gold','Platinum','name','Silver','Bronze','email')
+             ->get();
+
+        return view('awards.costumedesigner',compact('data'));
+    }
 }
